4f55e3b035c9a1a20d52560f4762575078f0a272
axn337 <axn337@case.edu>
2022-05-31 13:37:30 -0400

    add and fix image subscriber

--- src/gray_code_pattern_capture.cpp
+++ src/gray_code_pattern_capture.cpp
@@ -1,24 +1,42 @@
 #include <ros/ros.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui.hpp>
 #include <opencv2/structured_light.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/videoio.hpp>
+#include <opencv2/opencv.hpp>
 #include <iostream>
 #include <stdio.h>
+#include <cv_bridge/cv_bridge.h>
+
 
 using namespace cv;
 using namespace std;
+
+bool got_image;
+cv::Mat img;
+
+void imageCB(const sensor_msgs::Image::ConstPtr& im)
+{
+  ROS_INFO("callback");
+
+  cv_bridge::CvImagePtr cv_ptr;
+	try {
+		cv_ptr = cv_bridge::toCvCopy(im);
+		img = cv_ptr->image;
+		got_image = true;
+		return;
+	} catch (cv_bridge::Exception &e) {
+		ROS_ERROR("Could not convert from encoding to 'bgr8'.");
+		return;
+	}
+}
 
 int main( int argc, char** argv )
 {
   ros::init(argc, argv, "gray_code_pattern_capture");
 
-  structured_light::GrayCodePattern::Params params;
+  structured_light::GrayCodePattern::Params params; 
 
   // projector resolution
-  int proj_height= 400;
-  int proj_width= 400;
+  int proj_height= 300;
+  int proj_width= 300;
 
   string path="/home/ammarnahari/ros_ws/src/dvrk_structure_light/img/";
 
@@ -42,23 +60,44 @@ int main( int argc, char** argv )
   namedWindow("Pattern Window",WINDOW_NORMAL);
   resizeWindow("Pattern Window",params.width
   ,params.height);
-  moveWindow("Pattern Window",1920+150,500);
+  moveWindow("Pattern Window",600,500);
 
-  VideoCapture cam(0);
-  Mat frame;
-  namedWindow("view",cv::WINDOW_AUTOSIZE);
+  ros::NodeHandle nh;
+  ros::Subscriber sub = nh.subscribe("/amscope/image_raw", 1, imageCB);
+  ROS_INFO("created subscribier, spinning...");
 
+
+  //grap and write loop
   int i = 0;
   while( i < (int) pattern.size() )
   {
-    cout << "Waiting to save image number " << i + 1 << endl << "Press any key to acquire the photo" << endl;
+    cout << "Display pattern " << i << endl;
+
     imshow( "Pattern Window", pattern[i] );
-    cam>>frame;
-    imshow("view",frame);
-    waitKey(0);
+    got_image = false;
+
+    int count_image_cb=0;
+    while(!got_image&&ros::ok){
+      ros::spinOnce();
+      ROS_WARN_STREAM("Waiting for image data "<<count_image_cb);
+      ros::Duration(0.1).sleep();
+      count_image_cb++;
+    }
+
+    if(img.empty())
+    {
+      ROS_ERROR("Could not convert the image");
+      return 1;
+    }
+
+
+    // imshow("view",img);
+
+    std::string savingName = "/home/ammarnahari/ros_ws/src/dvrk_structure_light/img/" + std::to_string(i) + ".jpg";
+    imwrite(savingName,img);
+    if (waitKey(1) <= 0) break;
+    
     i++;
   }
-  cam.release();
-  // the camera will be deinitialized automatically in VideoCapture destructor
   return 0;
 }
