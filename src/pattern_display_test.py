ce2d0878a48d2f8e8b6c0257cbe480b028d4811c
axn337 <axn337@case.edu>
2023-03-20 16:25:46 -0400

    tuning of projection on surgical scene

--- src/pattern_display_test.py
+++ src/pattern_display_test.py
@@ -6,16 +6,19 @@ import cv2
 def main():
     rospy.init_node("pattern_display")
 
+    size=100
+
     img_path = "/home/axn337/Downloads/black_white_stripes.jpg"  # add path to the image
-    image1 = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    image1 = cv2.resize(cv2.imread(img_path, cv2.IMREAD_COLOR),(size,size))
+
 
     if image1 is None:
         rospy.loginfo("Could not read the image: %s", img_path)
         return 1
 
     cv2.namedWindow("pattern_out", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("pattern_out", 500, 500)
-    cv2.moveWindow("pattern_out", 750, 574)
+    cv2.resizeWindow("pattern_out", size, size)
+    cv2.moveWindow("pattern_out", 1080, 650) #1280x800/1024
 
     cv2.imshow("pattern_out", image1)
     cv2.waitKey(0)
